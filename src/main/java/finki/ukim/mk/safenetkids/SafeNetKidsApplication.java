9f3f41945beede8bf8ccb5aa723e7dbc9f0e6941
ljupce99 <ljupcejovanov99@gmail.com>
2026-04-10 00:29:04 +0200

    first commit

---
+++ src/main/java/finki/ukim/mk/safenetkids/SafeNetKidsApplication.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.safenetkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeNetKidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeNetKidsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/finki/ukim/mk/safenetkids/SafeNetKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.safenetkids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeNetKidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
